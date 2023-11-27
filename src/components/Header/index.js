9f478c558ccd25b06ed588e0d152c58355b9c479
Beatriz Gonçalves <68661352+biacrisg@users.noreply.github.com>
2023-11-27 15:17:33 -0300

    alters

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,7 @@ const Header = () => {
       <header>
         <a href="https://github.com/biacrisg" target="_blank" rel="noopener noreferrer" >
         <FontAwesomeIcon icon={faGithub} size='2x'  />
+        
       </a>
       <a href="https://www.linkedin.com/in/beatriz-cristina-gon%C3%A7alves-4a4b609b/" target="_blank" rel="noopener noreferrer" >
         <FontAwesomeIcon icon={faLinkedin}  size='2x'/>
